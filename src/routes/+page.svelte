d27706915adb832bac7bf35a5033e758a87ca69d
olegpolin <140200108+olegpolin@users.noreply.github.com>
2024-07-18 11:27:43 -0400

    Update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,13 @@
-<h1>Afind</h1>
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+    <title>Afind</title>
+    <meta name="description" content="Discover, engage, and explore with Afind - your personalized guide to local adventures." />
+</svelte:head>
+
+<div class="flex flex-col items-center gap-4 py-32">
+    <h1 class="text-5xl text-center font-semibold">Afind</h1>
+    <h2 class="text-lg text-center">Discover, engage, and explore with Afind - your personalized guide to local adventures.</h2>
+</div>
