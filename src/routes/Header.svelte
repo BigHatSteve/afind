bac6c7e2fc8756437e54efda514fd08077c04689
olegpolin <140200108+olegpolin@users.noreply.github.com>
2024-07-18 11:31:12 -0400

    Update header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,9 +2,9 @@
     import { Button } from "$lib/components/ui/button";
 </script>
 
-<header class="flex flex-row justify-between items-center p-2">
+<header class="flex flex-row justify-between items-center p-4">
     <a href="/">
-        <img class="h-14 w-fit" src="/afind-logo.webp" width="1556" height="418" alt="Afind Logo" />
+        <img class="h-10 w-fit" src="/afind-logo.webp" width="1556" height="418" alt="Afind Logo" />
     </a>
     <nav>
         <Button variant="secondary" href="/about">About us</Button>
