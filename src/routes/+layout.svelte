40faecb9c10075db2e0198ee9e3ebfd31e120a39
olegpolin <140200108+olegpolin@users.noreply.github.com>
2024-07-18 11:11:45 -0400

    Add sonner component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
     import "../app.css";
     import Header from "./Header.svelte";
+
+    import { Toaster } from "$lib/components/ui/sonner";
 </script>
+
+<Toaster />
 
 <div class="max-w-[64rem] mx-auto">
     <Header />
