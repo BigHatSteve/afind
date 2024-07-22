df56aa0a6603f19355b407baa5359e173be62588
olegpolin <140200108+olegpolin@users.noreply.github.com>
2024-07-22 16:24:20 -0400

    Add how it works section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,3 +15,6 @@
     <p class="text-sm pt-4">Join our waitlist to be notified when Afind releases!</p>
     <WaitlistForm data={data.waitlistForm} />
 </div>
+
+<h2 class="text-2xl text-center font-semibold pb-2">How it works</h2>
+<p class="text-center">Afind is a cutting-edge mobile application designed to revolutionize the way users discover local businesses and attractions. Our unique approach leverages short-form, AI-driven video recommendations tailored to your preferences and location.</p>
