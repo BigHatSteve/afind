99ffeb960db31e167b2019c023b2ded14f8acf10
olegpolin <140200108+olegpolin@users.noreply.github.com>
2024-07-23 10:09:46 -0400

    Update carousel component

--- src/lib/components/ui/carousel/carousel-next.svelte
+++ src/lib/components/ui/carousel/carousel-next.svelte
@@ -23,10 +23,10 @@
 	{variant}
 	{size}
 	class={cn(
-		"absolute h-8 w-8 touch-manipulation rounded-full",
+		"h-8 w-8 touch-manipulation rounded-full",
 		$orientation === "horizontal"
-			? "-right-12 top-1/2 -translate-y-1/2"
-			: "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
+			? ""
+			: "rotate-90",
 		className
 	)}
 	disabled={!$canScrollNext}
