6d60302637aabaa8cab90e5ec90f1e0c111a2ccd
olegpolin <140200108+olegpolin@users.noreply.github.com>
2024-07-24 10:44:31 -0400

    Add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    
+</script>
+
+<svelte:head>
+    <title>About - Afind</title>
+    <meta name="description" content="About Afind - Discover, engage, and explore with Afind - your personalized guide to local adventures." />
+</svelte:head>
+
+<div class="flex flex-col items-center gap-8 my-16">
+    <h1 class="text-4xl font-semibold text-center">About Afind</h1>
+    <p class="text-xl text-center pb-16">Afind works to find hidden gems within their city through localized and short AI-driven videos. Within a chosen radius, the app recommends businesses or attractions that are tailored to the user's preference over time algorithmically.</p>
+    <h2 class="text-3xl font-medium text-center">FAQ</h2>
+</div>
