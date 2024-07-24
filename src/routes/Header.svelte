2eaa4afff030f6dddaab4fa9dd1c8db42e5b02e0
olegpolin <140200108+olegpolin@users.noreply.github.com>
2024-07-23 20:35:11 -0400

    Update logo

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     import { Button } from "$lib/components/ui/button";
+    import logo from "$lib/assets/afind-logo.webp";
 </script>
 
 <header class="flex flex-row justify-between items-center p-4">
-    <a href="/">
-        <img class="h-10 w-fit" src="/afind-logo.webp" width="1556" height="418" alt="Afind Logo" />
+    <a href="/" class="flex flex-row items-center gap-2">
+        <img class="w-10 h-10" src={logo} width="132" height="132" alt="Afind logo" />
+        <span class="text-2xl font-semibold">Afind</span>
     </a>
     <nav>
         <Button variant="secondary" href="/about">About us</Button>
